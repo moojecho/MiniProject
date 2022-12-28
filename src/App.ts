b6541c432bce6147bb98ebdb3623cb1282877ccf
moojecho <wpfh188@naver.com>
2022-12-28 15:14:02 +0900

    typescript 재설정 #2
    typescript 재설정 #2

--- src/App.ts
+++ src/App.ts
@@ -1,15 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import styled from "styled-components";
 import { MainPage } from "./pages";
-import LoginPage from "./pages/LoginPage";
-import SignUpPage from "./pages/SignUpPage";
-import Detail from "./components/detail/Detail";
 
 function App() {
   return (
     <DivApp>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element ={<MainPage />} />
       </Routes>
     </DivApp>
   );
--- src/components/Banner/type.ts
+++ src/components/Banner/type.ts
@@ -1,1 +1,3 @@
 export type TotalSlides = number;
+
+
